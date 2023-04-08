41e04e7c7d7e8f0061785070b00098cd08a208b8
SoyaNyan <nyan@soya.moe>
2023-04-08 19:51:51 +0900

    linting, prettier options changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersController } from './users/users.controller'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
